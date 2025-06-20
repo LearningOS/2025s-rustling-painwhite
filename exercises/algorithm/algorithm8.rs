0ef8b9460118cae0ce71ae16d7781828966b0800
jianbai <2910436467@qq.com>
2025-06-20 23:49:57 +0800

    6.20

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -2,7 +2,7 @@
 	queue
 	This question requires you to use queues to implement the functionality of the stac
 */
-// I AM NOT DONE
+
 
 #[derive(Debug)]
 pub struct Queue<T> {
@@ -68,14 +68,24 @@ impl<T> myStack<T> {
     }
     pub fn push(&mut self, elem: T) {
         //TODO
+        self.q2.enqueue(elem);
+        
+        // 将主队列q1的所有元素转移到q2
+        while let Ok(val) = self.q1.dequeue() {
+            self.q2.enqueue(val);
+        }
+        
+        // 交换q1和q2，使q1始终包含所有元素
+        std::mem::swap(&mut self.q1, &mut self.q2);
     }
     pub fn pop(&mut self) -> Result<T, &str> {
         //TODO
-		Err("Stack is empty")
+        
+		self.q1.dequeue().map_err(|_| "Stack is empty")
     }
     pub fn is_empty(&self) -> bool {
 		//TODO
-        true
+        self.q1.is_empty()
     }
 }
 
